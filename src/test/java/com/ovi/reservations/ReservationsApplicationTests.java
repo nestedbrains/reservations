ec1c250a73d2a4245a71a6a9becf700f701a16b6
nestedbrains <isiful.ovi@gmail.com>
2019-10-18 18:12:33 +0600

    Resolved default spring boot dependencies.

---
+++ src/test/java/com/ovi/reservations/ReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovi.reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
